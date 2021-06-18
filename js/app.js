9b6780887d72d0662ee1bd76c9af1e5d121942b3
Tristan Tinning <tristan_tinning@hotmail.com>
2021-06-18 14:43:03 +0100

    MVP complete, can append album to music list

---
+++ js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#new-music-form');
+    form.addEventListener('submit', handleNewForm);
+
+    const deleteAll = document.querySelector('#delete-botton');
+    deleteAll.addEventListener('click', handleDeleteAll);
+});
+
+const handleNewForm = function(event) {
+    event.preventDefault();
+    const albumList = document.querySelector('#album-list');
+    const albumListItem = document.createElement('li');
+    albumList.appendChild(albumListItem);
+    const artist = document.createElement('h2');
+    artist.textContent = event.target.artist.value;
+    albumListItem.appendChild(artist);
+    const album = document.createElement('h3');
+    album.textContent = event.target.album.value;
+    albumListItem.appendChild(album);
+    const genre = document.createElement('h4');
+    genre.textContent = event.target.genre.value;
+    albumListItem.appendChild(genre);
+    this.reset();
+}
+
+const handleDeleteAll = function() {
+    const list = document.querySelector('#album-list');
+    list.innerHTML = '';
+}
