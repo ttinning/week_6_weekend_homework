245ba763f399425990e400363ff7810cfc39e34d
Tristan Tinning <tristan_tinning@hotmail.com>
2021-06-19 11:14:11 +0100

    album tile css complete:

--- js/app.js
+++ js/app.js
@@ -8,20 +8,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const handleNewForm = function(event) {
     event.preventDefault();
+
     const albumList = document.querySelector('#album-list');
     const albumListItem = document.createElement('li');
     albumList.appendChild(albumListItem);
+
     const artist = document.createElement('h2');
     artist.textContent = event.target.artist.value;
     albumListItem.appendChild(artist);
+
     const album = document.createElement('h3');
     album.textContent = event.target.album.value;
     albumListItem.appendChild(album);
+
     const genre = document.createElement('h4');
     genre.textContent = event.target.genre.value;
     albumListItem.appendChild(genre);
+
     this.reset();
 }
+
+
 
 const handleDeleteAll = function() {
     const list = document.querySelector('#album-list');
